6bdaec5eeff38f7c3d8476e7af823b3507c8614f
Jual Rana <contactjual@gmail.com>
2024-12-25 23:38:26 +0600

    var dump

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+// var_dump() function use for cheking the variable type. It can take multiple paramiters.
+
+$my_array = array(3, 4, 5, 7, 7);
+var_dump($my_array[0]);
+
+$x = 10;
+var_dump($x);
+
+$yes = true;
+var_dump($yes);
+
+$nill = null;
+var_dump($nill);
+
+echo "<br>";
+
+var_dump($my_array, $x, $yes, $nill);
+
+
+?>
